4b20277f5c6ca9d4ed8b4e23a1d804af1ab109ba
Joshua Ramos <auhsojsomar@gmail.com>
2019-07-24 18:57:57 +0800

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,10 +5,12 @@ import { Navbar, Nav } from "react-bootstrap";
 export default function Navigation() {
 	return (
 		<Navbar bg="light" expand="lg">
-			<Navbar.Brand href="/">React-Bootstrap</Navbar.Brand>
+			<Navbar.Brand className="Brand">
+				<Link to="/">React-Bootstrap</Link>
+			</Navbar.Brand>
 			<Navbar.Toggle aria-controls="basic-navbar-nav" />
 			<Navbar.Collapse id="basic-navbar-nav">
-				<Nav className="mr-auto">
+				<Nav className="ml-auto">
 					<Link to="/">Home</Link>
 					<Link to="/about">Link</Link>
 				</Nav>
