9870a145093792fba201fce6a2ffd94c51342820
Joshua Ramos <auhsojsomar@gmail.com>
2019-07-24 12:42:32 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.054774adbe886ee6e3c29227ef1745b5.js"
+  "/precache-manifest.58bd643614ff9ddf1610d5cff712c3d1.js"
 );
 
 self.addEventListener('message', (event) => {
