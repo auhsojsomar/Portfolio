00643006b586506a0be2aa44e71118729130959d
Joshua Ramos <auhsojsomar@gmail.com>
2019-07-30 12:04:24 +0800

    commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
-import Home from "./views/Home";
-import About from "./views/About";
 import NotFound from "./views/NotFound";
+
+const Home = lazy(() => import("./views/Home"));
+const About = lazy(() => import("./views/About"));
 
 export default function App() {
 	return (
 		<Router>
-			<Navbar />
-			<Switch>
-				<Route path="/" exact component={Home} />
-				<Route path="/about" component={About} />
-				<Route component={NotFound} />
-			</Switch>
+			<Suspense fallback={"Loading..."}>
+				<Navbar />
+				<Switch>
+					<Route path="/" exact component={Home} />
+					<Route path="/about" component={About} />
+					<Route component={NotFound} />
+				</Switch>
+			</Suspense>
 		</Router>
 	);
 }
